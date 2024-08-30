ad36539b0fbdf125e5e982472910cf7c09bbbc19
Tayeshob <taye@paystandards.com>
2024-08-30 22:17:22 +0500

    initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,34 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        blue: "#3760ff",
+        "deep-blue": "#152461",
+        "dark-gray": "#505069",
+        "light-blue": "#88A0FF",
+        "light-gray": "#DCDCE1",
+        black: "#090C1A",
+        white: "#FFFFFF",
+      },
+      backgroundImage: {
+        "gradient-blue-deep-blue": "linear-gradient(to right, #3860FF, #152461)",
+        "gradient-light-blue-deep-blue": "linear-gradient(to right, #88A0FF, #152461)",
+        "gradient-blue-black": "linear-gradient(to right, #3860FF, #090C1A)",
+      },
+      // backgroundImage: {
+      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      //   "gradient-conic":
+      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      // },
+    },
+  },
+  plugins: [],
+};
+export default config;
