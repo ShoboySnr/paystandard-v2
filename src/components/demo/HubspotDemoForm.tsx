8af6ce74309b860795f93727842287bb7732ae63
Taye Shobajo <taye@paystandards.com>
2024-12-16 12:42:32 +0100

    features update 16

--- src/components/demo/HubspotDemoForm.tsx
+++ src/components/demo/HubspotDemoForm.tsx
@@ -8,23 +8,23 @@ const HubspotDemoForm = () => {
     // @ts-ignore
     if (window.hbspt) {
       // @ts-ignore
-      // window.hbspt.forms.create({
-      //   region: 'na1',
-      //   portalId: '46454267',
-      //   formId: '0b6432d6-150f-4e80-b7fa-b735657a0e79',
-      //   target: `#demo-page`,
-      // });
       window.hbspt.forms.create({
         region: 'na1',
         portalId: '46454267',
         formId: '0b6432d6-150f-4e80-b7fa-b735657a0e79',
-        target: `#product-page-form`,
-        fieldLabels: {
-          firstname: 'Full Name',
-          email: 'Work Email',
-          company: 'Company',
-        },
+        target: `#demo-page`,
       });
+      // window.hbspt.forms.create({
+      //   region: 'na1',
+      //   portalId: '46454267',
+      //   formId: '0b6432d6-150f-4e80-b7fa-b735657a0e79',
+      //   target: `#product-page-form`,
+      //   fieldLabels: {
+      //     firstname: 'Full Name',
+      //     email: 'Work Email',
+      //     company: 'Company',
+      //   },
+      // });
     }
     // }, 0);
   }, []);
